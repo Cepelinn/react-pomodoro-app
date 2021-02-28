a76939e114d2562b66a5d3fad0a430317ea9bdfd
Leonid <leonidlvi@gmail.com>
2021-02-28 13:49:07 +0300

    Lift up StartStop handle to App

--- src/App.js
+++ src/App.js
@@ -2,12 +2,45 @@ import './App.css';
 import Break from './components/Break';
 import Session from './components/Session';
 import TimeLeft from './components/TimeLeft';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 
 function App() {
-
-  
+  const [sessionLength, setSessionLength] = useState(1500);
   const [breakLength, setBreakLength] = useState(300);
+  const [currentSessionType, setCurrentSessionType] = useState("Session");
+  const [intervalId, setIntervalId] = useState(null);
+  const [timeLeft, setTimeLeft] = useState(sessionLength);
+
+  useEffect(() => {
+    setTimeLeft(sessionLength);
+  }, [sessionLength]);
+
+  const isStarted = intervalId !== null;
+
+  const handleStartStopClick = () => {
+    if (isStarted) {
+      clearInterval(intervalId);
+      setIntervalId(null);
+    } else {
+      const newIntervalId = setInterval(() =>{
+        setTimeLeft(prevTimeLeft =>{
+          const newTimeLeft = prevTimeLeft - 1;
+          if (newTimeLeft >= 0) {
+            return prevTimeLeft - 1;
+          }
+          if (currentSessionType === "Session") {
+            setCurrentSessionType("Break");
+            setTimeLeft(breakLength);
+          } else if ( currentSessionType === "Break") {
+            setCurrentSessionType("Session");
+            setTimeLeft(sessionLength);
+          } 
+        });
+      }, 1000);
+    setIntervalId(newIntervalId);
+    }
+  }
+
 
   const decrementBreakLengthByOneMinute = () => {
     const newBreakLength = breakLength - 60;
@@ -21,9 +54,6 @@ function App() {
   const incrementBreakLengthByOneMinute = () => {
     setBreakLength(breakLength + 60);
   }
-
-  
-  const [sessionLength, setSessionLength] = useState(1500);
 
   const decrementSessionLengthByOneMinute = () => {
     const newSessionLength = sessionLength - 60;
@@ -47,7 +77,11 @@ function App() {
         decrementBreakLengthByOneMinute = {decrementBreakLengthByOneMinute}
         incrementBreakLengthByOneMinute = {incrementBreakLengthByOneMinute}
       />
-      <TimeLeft breakLength={breakLength} sessionLength= {sessionLength} />
+      <TimeLeft 
+        timeLeft={timeLeft}
+        handleStartStopClick={handleStartStopClick}
+        sessionStartStopLabel={isStarted ? 'Stop' : 'Start'}
+      />
       <Session
         sessionLength = {sessionLength}
         decrementSessionLengthByOneMinute = {decrementSessionLengthByOneMinute}
--- src/components/TimeLeft.jsx
+++ src/components/TimeLeft.jsx
@@ -1,51 +1,17 @@
-import React, { useState, useEffect } from 'react';
 import moment from 'moment';
 import momentDurationFormatSetup from 'moment-duration-format';
 
 momentDurationFormatSetup(moment);
 
-const TimeLeft = ({ breakLength, sessionLength }) => {
-  const [currentSessionType, setCurrentSessionType] = useState("Session");
-  const [intervalId, setIntervalId] = useState(null);
-  const [timeLeft, setTimeLeft] = useState(sessionLength);
-
-  useEffect(() => {
-    setTimeLeft(sessionLength);
-  }, [sessionLength]);
-
-  const isStarted = intervalId !== null;
-
-  const handleStartStopClick = () => {
-    if (isStarted) {
-      clearInterval(intervalId);
-      setIntervalId(null);
-    } else {
-      const newIntervalId = setInterval(() =>{
-        setTimeLeft(prevTimeLeft =>{
-          const newTimeLeft = prevTimeLeft - 1;
-          if (newTimeLeft >= 0) {
-            return prevTimeLeft - 1;
-          }
-          //return prevTimeLeft;
-          if (currentSessionType === "Session") {
-            setCurrentSessionType("Break");
-            setTimeLeft(breakLength);
-          } else if ( currentSessionType === "Break") {
-            setCurrentSessionType("Session");
-            setTimeLeft(sessionLength);
-          } 
-        });
-      }, 1000);
-    setIntervalId(newIntervalId);
-    }
-  }
+const TimeLeft = ({ timeLeft, handleStartStopClick, sessionStartStopLabel }) => {
+  console.log(sessionStartStopLabel);
 
   const formatedTimeLeft = moment.duration(timeLeft, 's').format('mm:ss');
   return(
   <div>
     <h2>Time Left</h2>
     <p>{formatedTimeLeft}</p>
-    <button onClick={handleStartStopClick}>{isStarted ? 'Stop' : 'Start'}</button>
+    <button onClick={handleStartStopClick}>{sessionStartStopLabel}</button>
   </div>);
 }
 
